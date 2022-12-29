4d7b1f2992eb75a766e8dbbf2bfe20f9154f879c
Matt Nichols <mattnichols206@gmail.com>
2022-12-29 13:09:59 -0800

    Responsive LinkBar component for About page

---
+++ src/routes/(main)/about/LinkBar.svelte
@@ -0,0 +1,60 @@
+<div class="links">
+    <a href="/">Homepage</a>
+    <a href="https://xoxo.zone/@flatpickles">Mastodon</a>
+    <a href="https://instagram.com/flatpickles">Instagram</a>
+    <a href="https://github.com/flatpickles">GitHub</a>
+    <a href="https://man1.biz">LinkedIn</a>
+    <a href="/matt-nichols-resume.pdf">Resume</a>
+</div>
+
+<style lang="scss">
+    .links {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        gap: var(--padding);
+    }
+
+    a {
+        font-family: 'Hobeaux';
+        font-size: 1.2em;
+        text-decoration: none;
+        color: rgb(11, 85, 11);
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+
+    /*
+        Size and align links so that there are always the same
+        number on each wrapped line (2 or 3).
+
+        todo: mitigate magic numbers in here
+    */
+
+    @media ( max-width: 600px) {
+        a {
+            flex-basis: 25%;
+            &:nth-child(3n - 1) {
+                text-align: center;
+            }
+            &:nth-child(3n) {
+                text-align: right;
+            }
+        }
+    }
+
+    @media ( max-width: 350px) {
+        a {
+            flex-basis: 45%;
+            &:nth-child(odd) {
+                text-align: left;
+            }
+            &:nth-child(even) {
+                text-align: right;
+            }
+        }
+    }
+</style>
