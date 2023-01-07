42d162a670f8dda29c69dbe9f6f431bcd6682bd1
Matt Nichols <mattnichols206@gmail.com>
2023-01-07 14:41:23 -0800

    Maybe fixing body height on mobile

--- src/routes/media/MediaDisplay.svelte
+++ src/routes/media/MediaDisplay.svelte
@@ -30,8 +30,8 @@
 <style>
     .media-container {
         position: absolute;
-        width: 100vw;
-        height: 100vh;
+        width: 100%;
+        height: 100%;
 
         display: flex;
         flex-direction: column;
