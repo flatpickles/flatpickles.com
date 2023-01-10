ca0fb6b08cdcd33229b41b71005754b2986b37e1
Matt Nichols <mattnichols206@gmail.com>
2023-01-10 09:30:12 -0800

    Rename directory in git

---
+++ src/lib/components/HomeLink.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    export let opacity: string = "0.5";
+</script>
+
+<a href="/" style="--base-opacity: {opacity}">
+    <img src="/media/quick-pick.png" alt="Flat Pickles" />
+</a>
+
+<style>
+    img {
+        /* todo: resize asset, or use SVG */
+        display: block;
+        width: calc(var(--padding) * 1.5);
+        height: calc(var(--padding) * 1.5);
+    }
+    
+    a {
+        color: inherit;
+        text-decoration: none;
+        opacity: var(--base-opacity);
+        transition: opacity 0.3s ease-out;
+        font-size: 1.25em;
+        pointer-events: all;
+    }
+
+    a:hover {
+        opacity: 1;
+    }
+</style>
---
+++ src/lib/components/HomeLinkOverlay.svelte
@@ -0,0 +1,17 @@
+<script>
+    import HomeLink from "./HomeLink.svelte";
+</script>
+
+<div class="home-wrapper">
+    <HomeLink opacity=0.7 />
+</div>
+
+<style>
+    .home-wrapper {
+        position: fixed;
+        background-color: inherit;
+        top: var(--padding);
+        right: var(--padding);
+        border-radius: 50%;
+    }
+</style>
