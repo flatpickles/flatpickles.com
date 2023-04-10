c72911f740452895e04e8edb5fb870162ad1cd50
Matt Nichols <mattnichols206@gmail.com>
2023-04-10 14:42:06 -0700

    Convergence is 'done'

--- src/lib/content/external.ts
+++ src/lib/content/external.ts
@@ -3,153 +3,159 @@ import { type ProjectData, ProjectType } from '$lib/types';
 
 export const externalProjects: ProjectData[] = [
     {
-        'title': 'World Wide Webcam',
-        'url': 'https://wwweb.cam/',
-        'date': DateUtils.pacificDate('2023-03-06'),
-        'type': ProjectType.WebApp
+        title: 'Convergence',
+        url: 'https://convergence.guru/',
+        date: DateUtils.pacificDate('2023-04-10'),
+        type: ProjectType.WebApp,
     },
     {
-        'title': 'Aurora Vibes',
-        'url': 'http://sketchbook.flatpickles.com/#aurora-vibes',
-        'date': DateUtils.pacificDate('2023-02-15'),
-        'type': ProjectType.Live
+        title: 'World Wide Webcam',
+        url: 'https://wwweb.cam/',
+        date: DateUtils.pacificDate('2023-03-06'),
+        type: ProjectType.WebApp,
     },
     {
-        'title': 'flatpickles.com',
-        'url': 'https://github.com/flatpickles/flatpickles.com',
-        'date': DateUtils.pacificDate('2023-01-23'),
-        'type': ProjectType.WebApp
+        title: 'Aurora Vibes',
+        url: 'http://sketchbook.flatpickles.com/#aurora-vibes',
+        date: DateUtils.pacificDate('2023-02-15'),
+        type: ProjectType.Live,
     },
     {
-        'title': 'Face Replace',
-        'url': 'https://github.com/flatpickles/ML-Experiments/blob/main/Face_Replace.ipynb',
-        'date': DateUtils.pacificDate('2023-02-03'),
-        'type': ProjectType.Notebook
+        title: 'flatpickles.com',
+        url: 'https://github.com/flatpickles/flatpickles.com',
+        date: DateUtils.pacificDate('2023-01-23'),
+        type: ProjectType.WebApp,
     },
     {
-        'title': 'Ethereal Goop',
-        'url': 'http://sketchbook.flatpickles.com/#ethereal-goop',
-        'date': DateUtils.pacificDate('2022-10-29'),
-        'type': ProjectType.Live
+        title: 'Face Replace',
+        url: 'https://github.com/flatpickles/ML-Experiments/blob/main/Face_Replace.ipynb',
+        date: DateUtils.pacificDate('2023-02-03'),
+        type: ProjectType.Notebook,
     },
     {
-        'title': 'Mandelbrot Set Viewer',
-        'url': 'http://sketchbook.flatpickles.com/#mandelbrot-set',
-        'date': DateUtils.pacificDate('2022-09-29'),
-        'type': ProjectType.Live
+        title: 'Ethereal Goop',
+        url: 'http://sketchbook.flatpickles.com/#ethereal-goop',
+        date: DateUtils.pacificDate('2022-10-29'),
+        type: ProjectType.Live,
     },
     {
-        'title': 'Rectilinear',
-        'url': 'http://sketchbook.flatpickles.com/#rectilinear',
-        'date': DateUtils.pacificDate('2022-09-14'),
-        'type': ProjectType.Live
+        title: 'Mandelbrot Set Viewer',
+        url: 'http://sketchbook.flatpickles.com/#mandelbrot-set',
+        date: DateUtils.pacificDate('2022-09-29'),
+        type: ProjectType.Live,
     },
     {
-        'title': 'Sketchbook',
-        'url': 'http://sketchbook.flatpickles.com/#no-signal',
-        'date': DateUtils.pacificDate('2022-08-15'),
-        'type': ProjectType.WebApp
+        title: 'Rectilinear',
+        url: 'http://sketchbook.flatpickles.com/#rectilinear',
+        date: DateUtils.pacificDate('2022-09-14'),
+        type: ProjectType.Live,
     },
     {
-        'title': 'Glitchy',
-        'url': 'https://editor.isf.video/shaders/62bb5b57ad0a68001af75b57',
-        'date': DateUtils.pacificDate('2022-07-11'),
-        'type': ProjectType.VideoEffect
+        title: 'Sketchbook',
+        url: 'http://sketchbook.flatpickles.com/#no-signal',
+        date: DateUtils.pacificDate('2022-08-15'),
+        type: ProjectType.WebApp,
     },
     {
-        'title': 'GutenBard',
-        'url': 'https://gutenbard.vercel.app',
-        'date': DateUtils.pacificDate('2022-06-14'),
-        'type': ProjectType.WebApp
+        title: 'Glitchy',
+        url: 'https://editor.isf.video/shaders/62bb5b57ad0a68001af75b57',
+        date: DateUtils.pacificDate('2022-07-11'),
+        type: ProjectType.VideoEffect,
     },
     {
-        'title': 'Floppy Disk',
-        'url': 'http://sketchbook.flatpickles.com/#floppy-disk',
-        'date': DateUtils.pacificDate('2022-04-08'),
-        'type': ProjectType.Live
+        title: 'GutenBard',
+        url: 'https://gutenbard.vercel.app',
+        date: DateUtils.pacificDate('2022-06-14'),
+        type: ProjectType.WebApp,
     },
     {
-        'title': 'Flat Picks - Season 4',
-        'url': 'https://open.spotify.com/playlist/3hd6tpg5ZUsme35lzG48V2?si=0e059e9fa00e4384',
-        'date': DateUtils.pacificDate('2021-12-31'),
-        'type': ProjectType.Playlist
+        title: 'Floppy Disk',
+        url: 'http://sketchbook.flatpickles.com/#floppy-disk',
+        date: DateUtils.pacificDate('2022-04-08'),
+        type: ProjectType.Live,
     },
     {
-        'title': 'Music for Ryan & Jessica',
-        'url': 'https://paper.dropbox.com/doc/Music-for-Ryan-Jessica--BQSQtmtxGhjczmXJI3UuxdaZAQ-hhmATgKFzLjcsaqbxIE5x',
-        'date': DateUtils.pacificDate('2021-08-08'),
-        'type': ProjectType.Playlist
+        title: 'Flat Picks - Season 4',
+        url: 'https://open.spotify.com/playlist/3hd6tpg5ZUsme35lzG48V2?si=0e059e9fa00e4384',
+        date: DateUtils.pacificDate('2021-12-31'),
+        type: ProjectType.Playlist,
     },
     {
-        'title': 'What a Day Transcription',
-        'url': 'https://www.soundslice.com/slices/qfMDc/',
-        'date': DateUtils.pacificDate('2021-04-21'),
-        'type': ProjectType.Transcription
+        title: 'Music for Ryan & Jessica',
+        url: 'https://paper.dropbox.com/doc/Music-for-Ryan-Jessica--BQSQtmtxGhjczmXJI3UuxdaZAQ-hhmATgKFzLjcsaqbxIE5x',
+        date: DateUtils.pacificDate('2021-08-08'),
+        type: ProjectType.Playlist,
     },
     {
-        'title': 'Wobbly Video Effect',
-        'url': 'https://editor.isf.video/shaders/624d2c1efa14610014854030',
-        'date': DateUtils.pacificDate('2021-01-27'),
-        'type': ProjectType.VideoEffect
+        title: 'What a Day Transcription',
+        url: 'https://www.soundslice.com/slices/qfMDc/',
+        date: DateUtils.pacificDate('2021-04-21'),
+        type: ProjectType.Transcription,
     },
     {
-        'title': 'Warpy Video Effect',
-        'url': 'https://editor.isf.video/shaders/624d2c75fa14610014854058',
-        'date': DateUtils.pacificDate('2021-01-01'),
-        'type': ProjectType.VideoEffect
+        title: 'Wobbly Video Effect',
+        url: 'https://editor.isf.video/shaders/624d2c1efa14610014854030',
+        date: DateUtils.pacificDate('2021-01-27'),
+        type: ProjectType.VideoEffect,
     },
     {
-        'title': 'Flat Picks - Season 3',
-        'url': 'https://open.spotify.com/playlist/442CwkAO1CNU0TEnstZQc6?si=xABKoa-WS7S6vaI4RLHDIg',
-        'date': DateUtils.pacificDate('2021-10-10'),
-        'type': ProjectType.Playlist
+        title: 'Warpy Video Effect',
+        url: 'https://editor.isf.video/shaders/624d2c75fa14610014854058',
+        date: DateUtils.pacificDate('2021-01-01'),
+        type: ProjectType.VideoEffect,
     },
     {
-        'title': 'What\'s Old Is New Again',
-        'url': 'https://hic.af/objkt/527978',
-        'date': DateUtils.pacificDate('2021-11-10'),
-        'type': ProjectType.Video
+        title: 'Flat Picks - Season 3',
+        url: 'https://open.spotify.com/playlist/442CwkAO1CNU0TEnstZQc6?si=xABKoa-WS7S6vaI4RLHDIg',
+        date: DateUtils.pacificDate('2021-10-10'),
+        type: ProjectType.Playlist,
     },
     {
-        'title': 'Flat Picks - Season 2',
-        'url': 'https://open.spotify.com/playlist/5QB5698lV4DguTVmvCZY9W?si=JGKeOjC7TeS8hqx9lhnBlg',
-        'date': DateUtils.pacificDate('2019-10-12'),
-        'type': ProjectType.Playlist
+        title: "What's Old Is New Again",
+        url: 'https://hic.af/objkt/527978',
+        date: DateUtils.pacificDate('2021-11-10'),
+        type: ProjectType.Video,
     },
     {
-        'title': 'Flat Picks - Season 1',
-        'url': 'https://open.spotify.com/playlist/2FV9kc9KxDPRIPiUMgHtwv?si=wkkoRYEKQGKUTTijPh_Xcw',
-        'date': DateUtils.pacificDate('2018-09-29'),
-        'type': ProjectType.Playlist
+        title: 'Flat Picks - Season 2',
+        url: 'https://open.spotify.com/playlist/5QB5698lV4DguTVmvCZY9W?si=JGKeOjC7TeS8hqx9lhnBlg',
+        date: DateUtils.pacificDate('2019-10-12'),
+        type: ProjectType.Playlist,
     },
     {
-        'title': 'Birdcrime',
-        'url': 'https://birdcrime.vercel.app',
-        'date': DateUtils.pacificDate('2017-12-03'),
-        'type': ProjectType.WebApp
+        title: 'Flat Picks - Season 1',
+        url: 'https://open.spotify.com/playlist/2FV9kc9KxDPRIPiUMgHtwv?si=wkkoRYEKQGKUTTijPh_Xcw',
+        date: DateUtils.pacificDate('2018-09-29'),
+        type: ProjectType.Playlist,
     },
     {
-        'title': 'Balls',
-        'url': 'https://man1-balls.vercel.app',
-        'date': DateUtils.pacificDate('2014-09-22'),
-        'type': ProjectType.WebApp
+        title: 'Birdcrime',
+        url: 'https://birdcrime.vercel.app',
+        date: DateUtils.pacificDate('2017-12-03'),
+        type: ProjectType.WebApp,
     },
     {
-        'title': 'Line Fractal Generator',
-        'url': 'https://line-fractals.vercel.app',
-        'date': DateUtils.pacificDate('2013-07-22'),
-        'type': ProjectType.WebApp
+        title: 'Balls',
+        url: 'https://man1-balls.vercel.app',
+        date: DateUtils.pacificDate('2014-09-22'),
+        type: ProjectType.WebApp,
     },
     {
-        'title': 'Helicopter Game',
-        'url': 'https://js-heli.vercel.app',
-        'date': DateUtils.pacificDate('2013-04-16'),
-        'type': ProjectType.WebApp
+        title: 'Line Fractal Generator',
+        url: 'https://line-fractals.vercel.app',
+        date: DateUtils.pacificDate('2013-07-22'),
+        type: ProjectType.WebApp,
     },
     {
-        'title': 'WebGL Fireworks',
-        'url': 'http://www.goldberg.io/fireworks/',
-        'date': DateUtils.pacificDate('2012-01-10'),
-        'type': ProjectType.WebApp
-    }
+        title: 'Helicopter Game',
+        url: 'https://js-heli.vercel.app',
+        date: DateUtils.pacificDate('2013-04-16'),
+        type: ProjectType.WebApp,
+    },
+    {
+        title: 'WebGL Fireworks',
+        url: 'http://www.goldberg.io/fireworks/',
+        date: DateUtils.pacificDate('2012-01-10'),
+        type: ProjectType.WebApp,
+    },
 ];
