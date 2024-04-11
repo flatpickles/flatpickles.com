db13d3c947bd4cac0eae85adf94220368430b369
Matt Nichols <mattnichols206@gmail.com>
2024-04-10 21:16:42 -0700

    Remove /notes backlink for now

--- src/routes/(main)/note/[slug]/+page.svelte
+++ src/routes/(main)/note/[slug]/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import HomeLinkOverlay from '$lib/components/HomeLinkOverlay.svelte';
-    import type { NoteData } from '$lib/types';
     import NoteDisplay from '$lib/components/NoteDisplay.svelte';
     import type { PageData } from './$types';
 
@@ -17,24 +16,4 @@
 
 <div class="note-wrapper">
     <NoteDisplay {note} exclusive={true} />
-
-    <nav class="back">
-        <a href="/notes">[See all Notes]</a>
-    </nav>
 </div>
-
-<style>
-    a {
-        color: var(--transparent-dark-text);
-    }
-
-    a:hover {
-        color: var(--link-color);
-    }
-
-    .back {
-        margin-top: 0.5em;
-        text-align: center;
-        font-size: 1.1em;
-    }
-</style>
