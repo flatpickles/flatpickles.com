8b2833ae06db5d135af5e3003f1a31b905b6fbb0
Matt Nichols <mattnichols206@gmail.com>
2024-10-31 14:13:27 -0700

    Hover marker for homepage

--- src/lib/components/ProjectList.svelte
+++ src/lib/components/ProjectList.svelte
@@ -3,11 +3,28 @@
     import type { ProjectData } from '$lib/types';
 
     export let projects: ProjectData[];
+    let hoverVisible = false;
+
+    // Function to set the icon position based on the hovered row
+    function setIconPosition(event: MouseEvent) {
+        const projectRow = event.currentTarget as HTMLElement;
+        const projectList = document.querySelector('.project-list');
+        const hoverIcon = document.querySelector('.hover-icon');
+
+        if (projectRow && projectList && hoverIcon) {
+            const rowRect = projectRow.getBoundingClientRect();
+            const listRect = projectList.getBoundingClientRect();
+            const offsetTop = rowRect.top - listRect.top;
+            (hoverIcon as HTMLElement).style.top = `${offsetTop}px`;
+        }
+        hoverVisible = true;
+    }
 </script>
 
-<div class="project-list">
+<div class="project-list" on:mouseleave={() => hoverVisible = false}>
     {#each projects as project}
-        <a href={project.url} class="project-row">
+        <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+        <a href={project.url} class="project-row" on:mouseover={setIconPosition}>
             <div class="project-title">
                 {project.title}
             </div>
@@ -21,6 +38,7 @@
             </div>
         </a>
     {/each}
+    <img src="/img/fp-icon.svg" alt="hover icon" class="hover-icon" class:visible={hoverVisible} />
 </div>
 
 <style>
@@ -30,13 +48,14 @@
     }
 
     .project-list {
+        position: relative;
         display: flex;
         flex-direction: column;
-
         font-size: 1.2em;
     }
 
     .project-row {
+        position: relative;
         padding: 0.7em;
         border-radius: 0.5em;
         display: flex;
@@ -62,7 +81,6 @@
     .project-title {
         flex-basis: 300px;
         flex-grow: 2;
-
         font-weight: 500;
         white-space: nowrap;
         overflow: hidden;
@@ -72,12 +90,43 @@
     .project-metadata {
         flex-basis: 300px;
         flex-grow: 1;
-
         display: flex;
         flex-direction: row;
         column-gap: 1em;
         justify-content: space-between;
-
         color: var(--transparent-dark-text);
+    }
+
+    .hover-icon {
+        /* todo: positioning is too magical in here; needs rework with rem units */
+
+        pointer-events: none;
+        position: absolute;
+        box-sizing: content-box;
+        left: -2.8em;
+        top: 0;
+
+        display: block;
+        width: 1.5em;
+        padding: 0.6em;
+
+        animation: rotate 3s linear infinite;
+        transition: opacity 0.3s ease, top 0.3s ease;
+        opacity: 0;
+    }
+
+    @media (min-width: 800px) and (hover: hover) {
+        .hover-icon.visible {
+            opacity: 1;
+        }
+    }
+
+    @keyframes rotate {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
     }
 </style>
